bdaa12a22b4a4f8ada92981a0263c697a2b07461
Arvinder-Singh-Ghumman <arvindersinghghumman@gmail.com>
2025-02-14 11:06:39 -0700

    frontend

--- src/Tools/encryption.tsx
+++ src/Tools/encryption.tsx
@@ -1,10 +1,6 @@
 import CryptoJS from 'crypto-js';
 
-// 32-byte key (must be exactly 32 characters for AES-256)
-// const SECRET_KEY = CryptoJS.enc.Utf8.parse("0123456789abcdef0123456789abcdef"); 
 const SECRET_KEY = process.env.SECRET_KEY
-// 16-byte IV (must be exactly 16 characters)
-// const IV = CryptoJS.enc.Utf8.parse("abcdef9876543210"); 
 const IV = process.env.IV; 
 
 // Encrypt function
