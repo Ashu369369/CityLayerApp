093090a458f782eb7f6a42242c5507d1a3c09c6f
Ashu369369 <aarshsingh95@gmail.com>
2025-02-20 18:41:38 -0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
-=======
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignupPage from './screens/Signup';
->>>>>>> e666f54f8e71e03be58d3d47b6fb73a97d697eee
+import { StyleSheet, Text, View } from "react-native";
+import SignupPage from "./screens/Signup";
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <ActivityIndicator />
-=======
-      <SignupPage/>
->>>>>>> e666f54f8e71e03be58d3d47b6fb73a97d697eee
+      <SignupPage />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,15 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
+    width: "100%",
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-=======
-    width:'100%',
-    backgroundColor: '#fff',
     // alignItems: 'center',
-    justifyContent: 'center',
->>>>>>> e666f54f8e71e03be58d3d47b6fb73a97d697eee
+    justifyContent: "center",
   },
 });
