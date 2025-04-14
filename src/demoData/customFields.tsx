196cf8a18cc160ff9b0e220182f18a6d14e86dbc
arvindersinghghumman <you@example.com>
2025-04-14 00:20:29 -0600

    customFields

---
+++ src/demoData/customFields.tsx
@@ -0,0 +1,54 @@
+export const customfieldproject = [
+  {
+    "customId": "1",
+    "projectId": "1",
+    "fieldName": "Client Name",
+    "fieldValue": "Acme Corp"
+  },
+  {
+    "customId": "2",
+    "projectId": "1",
+    "fieldName": "Start Date",
+    "fieldValue": "2024-05-10"
+  },
+  {
+    "customId": "3",
+    "projectId": "2",
+    "fieldName": "Project Manager",
+    "fieldValue": "John Doe"
+  },
+  {
+    "customId": "4",
+    "projectId": "2",
+    "fieldName": "Budget",
+    "fieldValue": "$50,000"
+  }
+]
+
+export const customfeildprogram = [
+  {
+    "customId": "1",
+    "programId": "1",
+    "fieldName": "Accreditation Body",
+    "fieldValue": "ABET"
+  },
+  {
+    "customId": "2",
+    "programId": "1",
+    "fieldName": "Curriculum Version",
+    "fieldValue": "2024 v2"
+  },
+  {
+    "customId": "3",
+    "programId": "2",
+    "fieldName": "Lead Instructor",
+    "fieldValue": "Prof. Sarah Lee"
+  },
+  {
+    "customId": "4",
+    "programId": "2",
+    "fieldName": "Enrollment Cap",
+    "fieldValue": "60"
+  }
+]
+
