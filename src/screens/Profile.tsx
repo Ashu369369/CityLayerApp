fdfcbe9a2a1eec818876a63652b975ebba84a700
arvindersinghghumman <you@example.com>
2025-04-13 20:46:37 -0600

    date Time fix  signup.tsx

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -158,7 +158,7 @@ const ProfileScreen: React.FC = () => {
               </View>
             </View>
             <Text style={styles.dob}>
-              Date of Birth: {formatDate(user.dob, dateFormat)}
+              Date of Birth: {user.dob ? formatDate(user.dob, dateFormat) : "N/A"}
             </Text>
             <Text style={styles.role}>
               Role:{" "}
